6ae2163a5ec789705aae1a56852e9fd663a26783
pavbox <pavboxdev@gmail.com>
2018-06-04 00:48:24 +0300

    added default stack class with some methods

--- task4/task3.cpp
+++ task4/task3.cpp
@@ -27,7 +27,6 @@
 
   Входные значения для тестирования:
 
-
 */
 
 #include <iostream>
@@ -38,8 +37,129 @@
 
 using namespace std;
 
+const int STACK_SIZE = 1024;
+
+enum BracketsType: char {
+  circle = '(',
+  square = '[',
+  braces = '{'
+};
+
+enum ErrorType {
+  badClosed, // wrong closing of brackets
+  unclosed, // catch if user not closed all of brackets
+  overflow, // catch if while stack overflow
+  underflow // catch while trying to pop while stack is empty (may be trying to close not opened brackets)
+};
+
+class BracketsStack {
+private:
+  BracketsType stack[STACK_SIZE];
+  int topIndex = -1; // is empty by default
+
+  /// Support methods
+
+  int isOverflow() {
+    return (topIndex >= STACK_SIZE);
+  }
+
+public:
+
+  /// Operations /w Stack
+
+  void push(BracketsType newType) {
+    if (!isOverflow()) {
+      stack[++topIndex] = newType;
+    } else {
+      std::cout << "Stack Overflow!" << '\n';
+    }
+  }
+
+  /// Support methods
+
+  int isEmpty() {
+    return (topIndex < 0) ? 1 : 0;
+  }
+
+  /// Output methods
+
+  void print() {
+    if (isEmpty()) {
+      std::cout << "Stack is empty!" << '\n';
+    } else {
+      std::cout << ": " << char(stack[0]) << '\n';
+    }
+  }
+};
+
+
+class Parser {
+private:
+  string rawString;
+  BracketsStack* bracketsStack;
+
+  // can pop this bracket?
+  // send error state
+
+  // find bracker -> check type -> add type to stack
+
+  int isCorrectSymbol(char symbol) {
+
+    if (symbol == '{' || symbol == '}') {
+
+    }
+
+    return 0;
+  }
+
+public:
+  Parser(string raw) {
+    rawString = raw;
+
+    bracketsStack = new BracketsStack();
+  }
+
+  void parse() {
+    // fill stack
+    // colored output
+    // show error if needed
+  }
+
+  void print() {
+    cout << rawString << endl;
+  }
+
+};
+
 int main(int argc, char const *argv[]) {
-
-
+  Parser* parser = new Parser("129ujasidj1");
+  parser->print();
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// imitate footer for scrolling of atom redactor
